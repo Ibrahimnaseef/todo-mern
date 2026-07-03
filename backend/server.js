baeea8f4691d10fd770c314260c7206c646822aa
Ibrahim Naseef K J <ibrahimnaseefkj@gmail.com>
2026-07-03 20:44:36 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from "dotenv";
+import dns from 'dns';
+import { connectDB } from './config/db.js';
+import Todo from './models/todo.model.js';
+import todoRoutes from "./routes/todo.routes.js"
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+dotenv.config();
+
+const app = express();
+app.use(express.json())
+// app.get("/", (req, res) => { 
+//     res.send("server ready");
+// });
+
+app.use("/api/todos",todoRoutes);
+
+
+app.listen(5000, () => {  
+    connectDB();
+    console.log("Server started at http://localhost:5000");
+});
