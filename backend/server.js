dc0566e3d26546b6df810869ff5611d7c2bbaa69
Ibrahim Naseef K J <ibrahimnaseefkj@gmail.com>
2026-07-05 19:16:24 +0530

    Fix build script typo2

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,13 @@ app.use("/api/todos",todoRoutes);
 const __dirname=path.resolve();
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"/frontend/dis")));
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
     });
 }
 
-app.listen(5000, () => {  
+app.listen(PORT, () => {  
     connectDB();
     console.log("Server started at http://localhost:5000");
 });
